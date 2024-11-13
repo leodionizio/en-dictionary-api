88ea2faa630640663ccde11b2a6796c2c1f1ef3d
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-11-13 18:34:15 -0300

    chore: cahange redis connection

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -9,9 +9,9 @@ import { CacheService } from './cache.service';
     ConfigModule.forRoot(),
     RedisModule.forRoot({
       config: {
-        host: process.env.REDIS_HOST || 'redis',
-        port: Number(process.env.REDIS_PORT) || 6379,
-        // password: process.env.REDIS_PASSWORD,
+        url: process.env.REDIS_URL,
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
       },
     }),
   ],
